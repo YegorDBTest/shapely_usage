412e80dab61d94223f24bdd34d31d41fc90718fb
Yegor Bitensky <yegorbitensky@gmail.com>
2021-08-19 23:46:46 +0300

    Add Geom class

--- python/app/src/main.py
+++ python/app/src/main.py
@@ -1,16 +1,32 @@
+from operator import attrgetter
+
+from statistics import fmean
+
 from shapely import affinity
 from shapely.geometry import box, MultiPolygon
 
 
-if __name__ == '__main__':
-    points = ((5, 5), (10, 5), (5, 10), (10, 10))
+class Geom:
 
-    polygons = MultiPolygon([
-        affinity.rotate(
-            box(p[0] - 2, p[1] - 1, p[0] + 2, p[1] + 1),
-            30
-        ) for p in points
-    ])
+    def __init__(self, x, y, width, height, angle):
+        self.angle = angle
+        self.polygon = affinity.rotate(
+            box(x - width / 2, y - height / 2, x + width / 2, y + height / 2),
+            angle
+        )
+
+
+if __name__ == '__main__':
+    items = (
+        Geom(5, 5, 4, 2, 30.0),
+        Geom(10, 5, 4, 2, 31.6),
+        Geom(5, 10, 4, 2, 28.2),
+        Geom(10, 10, 4, 2, 36.7),
+    )
+
+    polygons = MultiPolygon(map(attrgetter('polygon'), items))
+
+    average_angle = fmean(map(attrgetter('angle'), items))
 
     print(polygons.wkt)
-    print(affinity.rotate(polygons, -30).wkt)
+    print(affinity.rotate(polygons, -average_angle).wkt)
