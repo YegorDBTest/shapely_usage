0e68050a86892f5002b218bff466b6d60a38f6b2
Yegor Bitensky <yegorbitensky@gmail.com>
2021-08-23 22:58:16 +0300

    Add GeomLine class

--- python/app/src/main.py
+++ python/app/src/main.py
@@ -6,6 +6,22 @@ from shapely import affinity
 from shapely.geometry import box, MultiPolygon
 
 
+class GeomLine:
+
+    def __init__(self, points):
+        self.points = points
+
+    def __gt__(self, other):
+        sum1 = self.points[0][0] + self.points[1][0]
+        sum2 = other.points[0][0] + other.points[1][0]
+        return sum1 > sum2
+
+    def __eq__(self, other):
+        sum1 = self.points[0][0] + self.points[1][0]
+        sum2 = other.points[0][0] + other.points[1][0]
+        return sum1 == sum2
+
+
 class Geom:
 
     def __init__(self, x, y, width, height, angle):
@@ -28,6 +44,14 @@ class Geom:
             and self.polygon.bounds[1] == other.polygon.bounds[1]
         )
 
+    def get_lines(self):
+        coords = tuple(self.polygon.boundary.coords)
+        lines = [GeomLine(coords[i : i + 2]) for i in range(4)]
+        return {
+            'back': min(*lines),
+            'front': max(*lines),
+        }
+
 
 if __name__ == '__main__':
     items = (
@@ -40,10 +64,13 @@ if __name__ == '__main__':
     mp = MultiPolygon(map(attrgetter('polygon'), items))
     average_angle = fmean(map(attrgetter('angle'), items))
     rotated_mp = affinity.rotate(mp, -average_angle)
-    print(rotated_mp.wkt)
+    # print(rotated_mp.wkt)
 
     for i, pg in enumerate(rotated_mp.geoms):
         items[i].polygon = pg
 
     for item in sorted(items):
-        print(item.polygon.wkt)
+        # print(item.polygon.wkt)
+        lines = item.get_lines()
+        print('back', lines['back'].points)
+        print('front', lines['front'].points)
